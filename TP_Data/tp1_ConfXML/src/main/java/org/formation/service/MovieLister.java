65edb11af3d7d9f325390d0f8ba0dd6ebe847602
David THI <david.thibau@gmail.com>
2022-02-20 19:33:58 +0100

    Initial commit

---
+++ TP_Data/tp1_ConfXML/src/main/java/org/formation/service/MovieLister.java
@@ -0,0 +1,15 @@
+package org.formation.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.formation.model.Movie;
+
+
+public class MovieLister {
+
+	// A impl�menter
+	public List<Movie> moviesDirectedBy(String director) {
+		return new ArrayList<>();
+	}
+}
